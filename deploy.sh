5f851a8e7a3ea0a37b0a492be913a591cbc4e8e5
phpman <phpman.song@gmail.com>
2021-09-19 12:39:07 +0800

    improve deploy process

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,26 @@
 #/bin/sh
+
 #works only for  mac 
+
 echo start ..
-cp -r html assets logs uploads config.json.default iris_blog.sql ../deploy/
-cp config.json.default ../deploy/config.json
+
+deploy_path=../deploy
+mkdir -p $deploy_path
+cp -r html assets logs uploads iris_blog.sql config.json.default $deploy_path
+
+cp $deploy_path/config.json.default  $deploy_path/config.json
+
+
 go env -w CGO_ENABLED=0 GOOS=linux GOARCH=amd64
-go build -o deploy/iris_blog_linux
+go build -o $deploy_path/iris_blog_linux
 
 go env -w CGO_ENABLED=0 GOOS=windows GOARCH=amd64
-go build -o deploy/iris_blog_windows.exe
+go build -o $deploy_path/iris_blog_windows.exe
 
 go env -w CGO_ENABLED=0 GOOS=darwin GOARCH=amd64
+go build -o $deploy_path/iris_blog_darwin
+
+zip -q -r iris_blog`date +%Y%m%d%H%M`.zip $deploy_path
 
 echo finished ..
 
